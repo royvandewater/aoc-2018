46a2aa7d5788ad91e9e1e4a63b624fa904d5d9dd
Roy van de Water <roy.v.water@gmail.com>
2023-07-11 10:00:59 -0400

    Add comments

--- day1/b.pl
+++ day1/b.pl
@@ -3,73 +3,41 @@
 :- initialization(main, main).
 
 main :-
-  open('input.txt', read, In),
+  open('example1.txt', read, In),
   input_to_adjustments(In, Adjustments),
-  % write("Adjustments: "), writeln(Adjustments),
   find_first_repeated_frequency(Adjustments, FirstRepeatedFrequency),
   write("First repeated frequency: "), writeln(FirstRepeatedFrequency),
   close(In).
 
+% Overload input_to_adjustments to provide the initialization value for the Accumulator (Acc)
 input_to_adjustments(In, Adjustments) :- input_to_adjustments(In, Adjustments, []).
 input_to_adjustments(In, Adjustments, Acc) :-
   (read_line_to_string(In, Line),
-  %  write("Line: "), writeln(Line),
+   % IF the line we just read is the EOF char
    (Line = end_of_file
+    % THEN our Accumulator contains our entire list of Adjustments
     -> Adjustments = Acc
+    % ELSE convert the line into a number, append it ot the list of adjustments, and recurse
     ;  (atom_number(Line, Value),
         append(Acc, [Value], NewAcc),
         input_to_adjustments(In, Adjustments, NewAcc)))).
 
 find_first_repeated_frequency(Adjustments, FirstRepeatedFrequency) :- find_first_repeated_frequency(Adjustments, FirstRepeatedFrequency, [0]).
-find_first_repeated_frequency([],          _FirstRepeatedFrequency, _Frequencies) :- writeln("Should be unreachable").
 find_first_repeated_frequency(Adjustments, FirstRepeatedFrequency, Frequencies) :-
+  % The head of Frequencies is always the most recently visited frequency
   ([LastFrequency | _] = Frequencies,
+   % Grab the first adjustment
    [Adjustment | Rest] = Adjustments,
+   % and push it to the end of the adjustment list like a ring buffer so that we can keep processing them indefinitely
    append(Rest, [Adjustment], NewAdjustments),
+   % calculate the new frequency by applying the adjustment to the last frequency
    Frequency is LastFrequency + Adjustment,
-  %  write("Frequencies: "), writeln(Frequencies),
-  %  write("Frequency: "), writeln(Frequency),
+
+   % IF we've already seen this frequency
    (memberchk(Frequency, Frequencies)
+    % THEN this is the first repeated frequency
     -> FirstRepeatedFrequency = Frequency
+    % ELSE we add this frequency to the head of the list of known frequencies and recurse
     ;  (NewFrequencies = [Frequency | Frequencies],
         find_first_repeated_frequency(NewAdjustments, FirstRepeatedFrequency, NewFrequencies)))).
 
-
-
-% find_answer(In, Answer) :-
-%   find_answer_r(In, [], Answer).
-
-% find_answer_r(In, Acc, Answer) :-
-%   (read_line_to_string(In, Line),
-%    write("Acc"), writeln(Acc),
-%    (Line = end_of_file
-%     -> writeln("EOF, no answer found")
-%     ;  (Frequency is compute_frequency()
-%         memberchk(Line, Acc)
-%         -> Answer = Line
-%         ; (append(Acc, [Line], NewAcc),
-%            find_answer_r(In, NewAcc, Answer))))).
-
-% compute_frequency(Adjustments, Frequency) :- compute_frequency(Adjustments, Frequency, 0)
-% compute_frequency([], Frequency, Acc) :- Frequency = Acc
-% compute_frequency(Adjustments, Frequency, Acc) :-
-%   ([Head | Rest] = Adjustments,
-%    NewAcc is Acc + Head,
-%    compute_frequency(Rest, Frequency, NewAcc)).
-
-
-  %  ;  writeln("No EOF")).
-  %  memberchk(Line, Acc)
-
-  %  write("IsMember: "),
-  %  writeln(IsMember),
-  %  Answer = Line).
-  %  -> (write("EOF reached: "),
-  %      write(Line),
-  %      Answer = "Could not find an answer")
-  %  ;  (memberchk(Line, Acc)
-  %      -> (Answer = Line,
-  %          write("Found an answer: "),
-  %          writeln(Line))
-  %      ;  (append(Acc, [Line], NewAcc),
-  %          find_answer_r(In, NewAcc, Answer)))).
